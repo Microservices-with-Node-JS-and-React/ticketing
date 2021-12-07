20f1501a5a5425c808199e0218fbefae9b952259
= <nikola.kolchakov@gmail.com>
2021-12-07 15:45:56 +0200

    create ticket + tests

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,22 +2,23 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@nkgittix/common';
+import { currentUser, errorHandler, NotFoundError } from '@nkgittix/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 
-// let nodejs trust the ingress-nginx proxy
 app.set('trust proxy', true);
 
 app.use(json());
 app.use(
   cookieSession({
-    // don't encrypt the JWT inside the cookie
     signed: false,
-    // if true, send through HTTPS only
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
