4a1aa437f78c514f8bbeef8387990ad572b5934f
= <nikola.kolchakov@gmail.com>
2021-12-09 19:03:33 +0200

    tests for Order creation

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,61 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { app } from '../../app';
+import { Order, OrderStatus } from '../../models/orders';
+import { Ticket } from '../../models/ticket';
+
+it('should return 404 error if ticket does NOT exist', async () => {
+  const ticketId = new mongoose.Types.ObjectId();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({
+      ticketId,
+    })
+    .expect(404);
+});
+
+it('should return 400 error if ticket is reserved', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+  const order = Order.build({
+    ticket,
+    userId: 'userId',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(400);
+});
+
+it('should reserve a ticket', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const createdOrderResponse = await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({
+      ticketId: ticket.id,
+    });
+
+  expect(createdOrderResponse.status).toEqual(201);
+  expect(createdOrderResponse.body.ticket.id).toEqual(ticket.id);
+});
+
+// examples of the rest of generic validations
+// are present in the ticket service
